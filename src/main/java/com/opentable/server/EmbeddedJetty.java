c1e0db941e3f5cbb852a71ceacfa36ccca687b2d
Steven Schlansker <sschlansker@opentable.com>
2016-04-29 15:36:25 -0700

    Correct Jetty bind port

--- src/main/java/com/opentable/server/EmbeddedJetty.java
+++ src/main/java/com/opentable/server/EmbeddedJetty.java
@@ -2,6 +2,7 @@ package com.opentable.server;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +10,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EmbeddedJetty {
+    @Value("${ot.http.bind-port:${PORT0:0}}")
+    int httpBindPort;
+
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
         JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory();
-        factory.setPort(9000);
+        factory.setPort(httpBindPort);
         factory.setSessionTimeout(10, TimeUnit.MINUTES);
         return factory;
     }
