c653e35b53460ee65dcd7cff06191870b756e978
Steven Schlansker <sschlansker@opentable.com>
2016-05-18 13:24:01 -0700

    Fix static resource handling

--- src/main/java/com/opentable/server/StaticResourceConfiguration.java
+++ src/main/java/com/opentable/server/StaticResourceConfiguration.java
@@ -4,7 +4,6 @@ import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.util.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,13 +13,13 @@ public class StaticResourceConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(StaticResourceConfiguration.class);
 
     @Bean
-    public FactoryBean<ServletRegistrationBean> staticResourceServlet() {
+    public ServletRegistrationBean staticResourceServlet() {
 
         final Resource rsrc = Resource.newClassPathResource("/static/");
 
         if (rsrc == null) {
             LOG.warn("Didn't find '/static' on classpath, not serving static files");
-            return new ServletFactoryBean(null);
+            return new ServletRegistrationBean();
         }
 
         DefaultServlet servlet = new DefaultServlet();
@@ -28,29 +27,7 @@ public class StaticResourceConfiguration {
         bean.addInitParameter("gzip", "true");
         bean.addInitParameter("etags", "true");
         bean.addInitParameter("resourceBase", rsrc.getURI().resolve("..").toString());
-        return new ServletFactoryBean(bean);
-    }
-
-    static class ServletFactoryBean implements FactoryBean<ServletRegistrationBean> {
-        private final ServletRegistrationBean bean;
-
-        ServletFactoryBean(ServletRegistrationBean bean) {
-            this.bean = bean;
-        }
-
-        @Override
-        public ServletRegistrationBean getObject() throws Exception {
-            return bean;
-        }
-
-        @Override
-        public Class<?> getObjectType() {
-            return ServletRegistrationBean.class;
-        }
-
-        @Override
-        public boolean isSingleton() {
-            return true;
-        }
+        LOG.info("Configuring static resources: {}", bean.getInitParameters());
+        return bean;
     }
 }
