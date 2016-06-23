b631e7d7b2042e87660115824f63c6e29cfef7e3
Steven Schlansker <sschlansker@opentable.com>
2016-06-23 16:13:31 -0700

    Print out property sources at application startup

--- src/main/java/com/opentable/server/ServerConfigConfiguration.java
+++ src/main/java/com/opentable/server/ServerConfigConfiguration.java
@@ -1,17 +1,38 @@
 package com.opentable.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+import com.opentable.server.ServerConfigConfiguration.LogAppConfig;
 import com.opentable.spring.ConversionServiceConfiguration;
 
 @Configuration
-@Import(ConversionServiceConfiguration.class)
+@Import({
+    ConversionServiceConfiguration.class,
+    LogAppConfig.class
+})
 public class ServerConfigConfiguration {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfig() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Component
+    static class LogAppConfig implements EnvironmentAware {
+        @Override
+        public void setEnvironment(Environment environment) {
+            Logger log = LoggerFactory.getLogger(LogAppConfig.class);
+            ((ConfigurableEnvironment) environment).getPropertySources().forEach(ps -> {
+                log.debug("Application uses property source {}", ps);
+            });
+        }
     }
 }
