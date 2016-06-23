da3f355e50d7a56e9bb069a6a1bde3f4b573b25d
Chris Pennello <cpennello@opentable.com>
2016-06-22 17:18:22 -0700

    Adds demo program for startup failed handler.
    Since this kills the process, I'm not sure how to do this as a unit
    test.

--- src/main/java/com/opentable/server/StartupFailedHandler.java
+++ src/main/java/com/opentable/server/StartupFailedHandler.java
@@ -2,14 +2,19 @@ package com.opentable.server;
 
 import java.time.Duration;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import org.springframework.boot.context.event.ApplicationFailedEvent;
 import org.springframework.context.ApplicationListener;
 
 import com.opentable.util.JvmFallbackShutdown;
 
 class StartupFailedHandler implements ApplicationListener<ApplicationFailedEvent> {
+    @VisibleForTesting
+    static final Duration timeout = Duration.ofSeconds(5);
+
     @Override
     public void onApplicationEvent(ApplicationFailedEvent event) {
-        JvmFallbackShutdown.fallbackTerminate(Duration.ofSeconds(5));
+        JvmFallbackShutdown.fallbackTerminate(timeout);
     }
 }
---
+++ src/test/java/com/opentable/server/StartupFailedDemo.java
@@ -0,0 +1,30 @@
+package com.opentable.server;
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class StartupFailedDemo {
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            SpringApplication.run(App.class, args);
+        } catch (BeanCreationException e) {
+            // No problem... wait for the T-1000.
+            Thread.sleep(StartupFailedHandler.timeout.toMillis() * 2);
+        }
+        throw new RuntimeException("should never be reached");
+    }
+
+    @Configuration
+    @RestHttpServer
+    public static class App {
+        @Configuration
+        public static class BustedConfiguration {
+            @Bean
+            public Object boom() {
+                throw new RuntimeException("boom");
+            }
+        }
+    }
+}
