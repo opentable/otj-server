49986827f15789e11ffcec23c0885029e237a0dc
Steven Schlansker <sschlansker@opentable.com>
2016-06-23 16:31:33 -0700

    Simplify RestHttpServerTest

--- src/test/java/com/opentable/server/RestHttpServerTest.java
+++ src/test/java/com/opentable/server/RestHttpServerTest.java
@@ -2,30 +2,20 @@ package com.opentable.server;
 
 import java.io.IOException;
 
-import javax.inject.Named;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.opentable.service.ServiceInfo;
 
 public class RestHttpServerTest {
+    private Client client;
     private ConfigurableApplicationContext context;
     private Integer port;
 
@@ -35,10 +25,12 @@ public class RestHttpServerTest {
         Assert.assertNull(port);
         context = SpringApplication.run(TestServer.class);
         port = context.getBeanFactory().getBean(HttpServerInfo.class).getPort();
+        client = ClientBuilder.newClient();
     }
 
     @After
     public void after() {
+        client = null;
         Assert.assertNotNull(context);
         Assert.assertNotNull(port);
         port = null;
@@ -49,14 +41,13 @@ public class RestHttpServerTest {
 
     @Test
     public void testHello() throws IOException {
-        Assert.assertEquals(TestServer.HELLO_WORLD, readString("/", null));
+        Assert.assertEquals(TestServer.HELLO_WORLD, request("/").readEntity(String.class));
     }
 
     @Test
     public void testMissing() throws IOException {
-        try (final CloseableHttpResponse resp = getResponse("/not/found/omg/wtf/bbq")) {
-            Assert.assertEquals(404, resp.getStatusLine().getStatusCode());
-        }
+        Response r = request("/not/found/omg/wtf/bbq");
+        Assert.assertEquals(404, r.getStatus());
     }
 
     @Test
@@ -69,29 +60,13 @@ public class RestHttpServerTest {
         testStatic("static/test.png", "image/png");
     }
 
-    private CloseableHttpResponse getResponse(final String path) throws IOException {
-        final CloseableHttpClient client = HttpClients.createMinimal();
-        final HttpGet get = new HttpGet("http://localhost:" + port + path);
-        return client.execute(get);
-    }
-
-    private byte[] readBytes(final String path, final String expectedContentType) throws IOException {
-        try (final CloseableHttpResponse resp = getResponse(path)) {
-            Assert.assertEquals(200, resp.getStatusLine().getStatusCode());
-            if (expectedContentType != null) {
-                Assert.assertEquals(expectedContentType, resp.getFirstHeader("Content-Type").getValue());
-            }
-            return IOUtils.toByteArray(resp.getEntity().getContent());
-        }
-    }
-
-    private String readString(final String path, final String expectedContentType) throws IOException {
-        return new String(readBytes(path, expectedContentType));
+    private Response request(final String path, final String ... expectedContentType) throws IOException {
+        return client.target("http://localhost:" + port).path(path).request(expectedContentType).get();
     }
 
     private void testStatic(final String path, final String expectedContentType) throws IOException {
         final byte[] expected = IOUtils.toByteArray(ClassLoader.getSystemResourceAsStream(path));
-        final byte[] actual = readBytes("/" + path, expectedContentType);
+        final byte[] actual = request(path, expectedContentType).readEntity(byte[].class);
         Assert.assertArrayEquals(expected, actual);
     }
 }
