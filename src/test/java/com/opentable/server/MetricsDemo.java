5c8fb32b69318c0e6f894a29d4bfd32ed5d4b6e6
Chris Pennello <cpennello@opentable.com>
2016-08-11 09:33:00 -0700

    Updates metrics demo given new app info usage.

--- src/test/java/com/opentable/server/MetricsDemo.java
+++ src/test/java/com/opentable/server/MetricsDemo.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.mock.env.MockEnvironment;
 
 import com.opentable.metrics.http.MetricsHttpConfiguration;
 
@@ -14,6 +15,8 @@ import com.opentable.metrics.http.MetricsHttpConfiguration;
  * Demonstration of serving Dropwizard Metrics AdminServlet.
  * Navigate to /metrics.
  * Uncomment in {@link #testProperties()} to test Graphite reporting.
+ * Note that if you want to test NMT metrics, you will need to add {@code -XX:NativeMemoryTracking=summary}
+ * to your JVM arguments.
  */
 @Configuration
 @Import({
@@ -29,6 +32,9 @@ public class MetricsDemo {
 
     private static Map<String, Object> testProperties() {
         final Map<String, Object> props = new HashMap<>();
+        props.put("INSTANCE_NO", "0");
+        props.put("OT_ENV_TYPE", "dev");
+        props.put("OT_ENV_LOCATION", "sf");
         //props.put("ot.graphite.graphite-host", "carbon-qa-sf.otenv.com");
         return Collections.unmodifiableMap(props);
     }
