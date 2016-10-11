22c9b79b56a7d94937721281820c59e4f1f1c8c0
Steven Schlansker <sschlansker@opentable.com>
2016-10-11 14:00:06 -0700

    tests: replace ServerTestBase with spring-boot-test

--- src/test/java/com/opentable/server/TestServer.java
+++ src/test/java/com/opentable/server/TestServer.java
@@ -3,22 +3,34 @@ package com.opentable.server;
 import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import com.opentable.jaxrs.JaxRsClientFactory;
+import com.opentable.jaxrs.StandardFeatureGroup;
 import com.opentable.service.ServiceInfo;
 
 @Configuration
 @RestHttpServer
-@Import(TestServer.TestResource.class)
+@Import({
+    TestServer.TestResource.class,
+    LoopbackRequest.class,
+})
 public class TestServer {
     public static final String HELLO_WORLD = "Hello, world!";
 
     public static void main(final String[] args) {
         SpringApplication.run(TestServer.class, args);
+    }
+
+    @Bean
+    @Named("test")
+    Client testClient(JaxRsClientFactory factory) {
+        return factory.newClient("test", StandardFeatureGroup.PLATFORM_INTERNAL);
     }
 
     @Configuration
