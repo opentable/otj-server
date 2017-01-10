e251844bd29c86482412793141d2f83305daea2f
Steven Schlansker <sschlansker@opentable.com>
2017-01-10 14:05:53 -0800

    OTApplication: allow optional configuration of SpringApplicationBuilder

--- src/main/java/com/opentable/server/OTApplication.java
+++ src/main/java/com/opentable/server/OTApplication.java
@@ -1,11 +1,43 @@
 package com.opentable.server;
 
+import java.util.function.Consumer;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * OpenTable specific Spring Boot style application runner.
+ * Sets up logging and other OT specific customizations.
+ *
+ * Note that this API only accepts {@code Class<?>} arguments
+ * rather than generic {@code Object} -- this is a style choice,
+ * we prefer to write explicit classes rather than relying on instances.
+ * This can change down the road if needed.
+ */
 public class OTApplication {
+    /**
+     * Construct and run a {@link SpringApplication} with the default settings for
+     * {code otj-} OpenTable Spring Boot based applications.
+     * @param applicationClass the main class to boot.  Should be a Spring configuration class.
+     * @param args the application arguments from main()
+     * @return the configured application context
+     */
     public static ConfigurableApplicationContext run(Class<?> applicationClass, String[] args) {
+        return run(applicationClass, args, b -> {});
+    }
+
+    /**
+     * Construct and run a {@link SpringApplication} with custom settings for
+     * {code otj-} OpenTable Spring Boot based applications.
+     * @param applicationClass the main class to boot.  Should be a Spring configuration class.
+     * @param args the application arguments from main()
+     * @return the configured application context
+     */
+    public static ConfigurableApplicationContext run(Class<?> applicationClass, String[] args, Consumer<SpringApplicationBuilder> customizer) {
         System.setProperty("org.springframework.boot.logging.LoggingSystem", "none");
-        return SpringApplication.run(applicationClass, args);
+        final SpringApplicationBuilder builder = new SpringApplicationBuilder(applicationClass);
+        customizer.accept(builder);
+        return builder.run(args);
     }
 }
