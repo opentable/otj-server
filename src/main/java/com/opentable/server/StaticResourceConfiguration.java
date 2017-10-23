bdba0077c6ce66da6860ffa2210741c28f3751bb
Chris Pennello <cpennello@opentable.com>
2017-10-23 10:06:06 -0700

    Makes static dir configurable.
    Jira: OTPL-1813

--- src/main/java/com/opentable/server/StaticResourceConfiguration.java
+++ src/main/java/com/opentable/server/StaticResourceConfiguration.java
@@ -1,5 +1,6 @@
 package com.opentable.server;
 
+import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -8,6 +9,7 @@ import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.util.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +20,27 @@ import org.springframework.context.annotation.Configuration;
 public class StaticResourceConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(StaticResourceConfiguration.class);
 
+    static final String DEFAULT_PATH_NAME = "static";
+    private static final String PATH_CONFIG_VALUE = "${ot.httpserver.static-path:" + DEFAULT_PATH_NAME + "}";
+
+    private final String staticPathName;
+
+    @Inject
+    StaticResourceConfiguration(@Value(PATH_CONFIG_VALUE) final String staticPathName) {
+        this.staticPathName = staticPathName;
+    }
+
+    public String staticPath() {
+        return staticPath("");
+    }
+
+    public String staticPath(final String rest) {
+        return String.format("/%s/%s", staticPathName, rest);
+    }
+
     @Bean
     public ServletRegistrationBean staticResourceServlet() {
-
-        final Resource rsrc = Resource.newClassPathResource("/static/");
+        final Resource rsrc = Resource.newClassPathResource(staticPath());
         LOG.debug("Found static resources at {}", rsrc);
 
         if (rsrc == null) {
@@ -35,10 +54,10 @@ public class StaticResourceConfiguration {
         }
 
         DefaultServlet servlet = new DefaultServlet();
-        ServletRegistrationBean bean = new ServletRegistrationBean(servlet, "/static/*");
+        ServletRegistrationBean bean = new ServletRegistrationBean(servlet, staticPath() + "*");
         bean.addInitParameter("gzip", "true");
         bean.addInitParameter("etags", "true");
-        bean.addInitParameter("resourceBase", StringUtils.substringBeforeLast(rsrc.toString(), "static"));
+        bean.addInitParameter("resourceBase", StringUtils.substringBeforeLast(rsrc.toString(), staticPathName));
         LOG.debug("Configuring static resources: {}", bean.getInitParameters());
         return bean;
     }
