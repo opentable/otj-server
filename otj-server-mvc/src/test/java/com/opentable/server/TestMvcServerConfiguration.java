14ff4cb1cf8407f1e480c46c641decb8aca11648
Dmitry Kaukov <dkaukov@opentable.com>
2018-10-24 13:50:51 +1100

    removed otj-rest-template dep. added message converters test

--- otj-server-mvc/src/test/java/com/opentable/server/TestMvcServerConfiguration.java
+++ otj-server-mvc/src/test/java/com/opentable/server/TestMvcServerConfiguration.java
@@ -2,28 +2,33 @@ package com.opentable.server;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.opentable.resttemplate.RestTemplateConfiguration;
-import com.opentable.resttemplate.RestTemplateFactory;
-import com.opentable.server.TestMvcServerConfiguration.MyResource;
 import com.opentable.service.ServiceInfo;
-import com.opentable.service.discovery.client.EnableDiscoveryClient;
 
 @Configuration
-@Import({RestTemplateConfiguration.class, MyResource.class})
-@EnableDiscoveryClient
+@Import(RestTemplateAutoConfiguration.class)
 @MVCServer
 public class TestMvcServerConfiguration {
 
@@ -33,8 +38,8 @@ public class TestMvcServerConfiguration {
     }
 
     @Bean
-    RestTemplate restTemplate(RestTemplateFactory factory) {
-        return factory.newTemplate();
+    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 
     public static void main(String[] args) {
@@ -53,8 +58,21 @@ public class TestMvcServerConfiguration {
         }
 
         @GetMapping("echo")
-        public EchoResponse echo() {
-            return restTemplate.getForEntity("https://postman-echo.com/get", EchoResponse.class).getBody();
+        public EchoResponse echo(@RequestHeader HttpHeaders headers) {
+            return restTemplate.exchange("https://postman-echo.com/get",
+                HttpMethod.GET,
+                new HttpEntity<>(null, headers),
+                EchoResponse.class).getBody();
+        }
+
+        @PostMapping("map")
+        public @ResponseBody MrBean map(@RequestBody MrBean req) {
+            return req;
+        }
+
+        @GetMapping("rsp")
+        public @ResponseBody MrBean rsp() {
+            return new MrBean("2", "1");
         }
     }
 
@@ -74,6 +92,25 @@ public class TestMvcServerConfiguration {
             return "EchoResponse [args=" + args + ", headers=" + headers + "]";
         }
 
+    }
+
+    public static class MrBean {
+
+        private final String foo, bar;
+
+        public String getFoo() {
+            return foo;
+        }
+
+        public String getBar() {
+            return bar;
+        }
+
+        @JsonCreator
+        public MrBean(String foo, String bar) {
+            this.foo = foo;
+            this.bar = bar;
+        }
     }
 
 }
