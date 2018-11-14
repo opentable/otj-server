0c1ca56ec7580f39fbc1ebfceed1f685822231b0
Dmitry Kaukov <dkaukov@opentable.com>
2018-11-14 16:15:23 +1100

    OTPL-2957 Added LoggingHandlerExceptionResolver to log stack traces
    It can be switched off by property "ot.server.mvc.log-resolved-exception"

--- otj-server-mvc/src/main/java/com/opentable/server/mvc/MVCHttpServerCommonConfiguration.java
+++ otj-server-mvc/src/main/java/com/opentable/server/mvc/MVCHttpServerCommonConfiguration.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.jboss.resteasy.util.Base64;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
@@ -40,12 +41,19 @@ import com.opentable.metrics.mvc.MetricsHttpMVCConfiguration;
     HealthHttpMVCConfiguration.class,
     MetricsHttpMVCConfiguration.class,
     CoreConservedHeadersConfiguration.class,
+    // Logging exception handler
+    LoggingHandlerExceptionResolver.class
 })
 class MVCHttpServerCommonConfiguration {
+
 
     // To make dependency checker happy.
     // We want spring-webmvc to be transitive here.
     public static final String URI_TEMPLATE_VARIABLES_ATTRIBUTE = HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE;
+
+    // To make dependency checker happy.
+    // We want jax-rs-services to be transitive here.
+    public static final int NO_OPTIONS = Base64.NO_OPTIONS;
 
     @Inject
     MVCHttpServerCommonConfiguration(ObjectMapper objectMapper, HttpMessageConverters httpMessageConverters) {
