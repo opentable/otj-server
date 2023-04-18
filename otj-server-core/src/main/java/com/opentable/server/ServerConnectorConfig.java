6adb478fe81248fec2a5521ab831d8cc784f5922
James Szczesny <jszczesny@opentable.com>
2023-04-17 22:01:45 -0700

    loadtest jetty w buff

--- otj-server-core/src/main/java/com/opentable/server/ServerConnectorConfig.java
+++ otj-server-core/src/main/java/com/opentable/server/ServerConnectorConfig.java
@@ -43,7 +43,7 @@ public interface ServerConnectorConfig {
     }
 
     default boolean isUseDirectBuffers() {
-        return false;
+        return true;
     }
 
     default boolean isAllowEmptySni() {
